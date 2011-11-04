946071617e461a57782f2cb637695db9c0893b70
Harm Aarts <harmaarts@gmail.com>
2011-11-04 23:06:04 +0100

    initial take

---
+++ cleck.rb
@@ -0,0 +1,12 @@
+require 'serialport'
+
+p = SerialPort.new("/dev/ttyUSB0")
+file = File.open("log/data.log", "a")
+pid = fork do
+  Signal.trap("TERM") do
+    file.close
+  end
+  while true do
+     file << p.readline
+  end
+end
