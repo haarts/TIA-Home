ca526c0edcab5754fae80bf170dc1d61ee9ae066
Harm Aarts <harmaarts@gmail.com>
2011-11-07 14:30:50 +0100

    add timestamp

--- cleck.rb
+++ cleck.rb
@@ -3,13 +3,14 @@ require 'serialport'
 p = SerialPort.new("/dev/ttyUSB0")
 file = File.open("log/data.log", "a")
 file.sync = true
-@stop = false
 pid = fork do
   Signal.trap("TERM") do
-    @stop = true
     file.close
   end
-  while not @stop do
-     file.write(p.readline)
+  while true do
+    begin
+      file.write(Time.now + "," + p.readline)
+    rescue IOError
+    end
   end
 end
