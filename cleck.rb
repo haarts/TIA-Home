e8ea869cff5b123fb71902217f9d8b13ad9a8d7b
Harm Aarts <harmaarts@gmail.com>
2011-11-07 14:12:11 +0100

    i think this wont work as well

--- cleck.rb
+++ cleck.rb
@@ -3,11 +3,13 @@ require 'serialport'
 p = SerialPort.new("/dev/ttyUSB0")
 file = File.open("log/data.log", "a")
 file.sync = true
+@stop = false
 pid = fork do
   Signal.trap("TERM") do
+    @stop = true
     file.close
   end
-  while true do
+  while not @stop do
      file.write(p.readline)
   end
 end
